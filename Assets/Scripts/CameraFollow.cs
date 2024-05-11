b0a80502f6b87f75ddecc4698af1d77296b2a954
RJPhenom <103343995+RJPhenom@users.noreply.github.com>
2024-05-11 14:45:02 -0400

    Update camera follow
    Updated camera follow script and prefab settings.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject ball;
-    public GameObject floor;
-
     public float offset;
-
-    private Collider floorCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        floorCollider = floor.GetComponent<Collider>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 floorPoint = floorCollider.ClosestPoint(ball.transform.position);
-        Vector3 dirToFloor = (ball.transform.position - floorPoint).normalized;
-        Debug.Log(floorPoint + ":::" + dirToFloor);
-        Vector3 adjustedPos = ball.transform.position + (dirToFloor * -offset);
+        Vector3 dirToWorldZero = (ball.transform.position - Vector3.zero).normalized;
 
-        transform.position = ball.transform.position + new Vector3(0, 10, -10);
+        transform.position = ball.transform.position + dirToWorldZero * offset;
     }
 }
