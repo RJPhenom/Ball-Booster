0ac26c7262dc0585f3f43f9fe64954ce2e0fd49b
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-05-11 01:46:55 -0400

    Created BoostTrigger and Renamed old to PickupBoostTrigger
    Created trigger that forces player in the direction of the path (by default)

---
+++ Assets/Scripts/PickupBoostTrigger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBoost : MonoBehaviour
+{
+    public float multiplier = 1.5f;
+    public float duration = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(Pickup(other));
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        // The player is the ball, but the thruster is manipulating it
+        GameObject thruster = GameObject.Find("Thruster");
+        // 
+        if (thruster != null)
+        {
+            Player stats = thruster.GetComponent<Player>();
+            if (stats != null)
+            {
+                stats.moveSpeed *= multiplier;
+
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<Collider>().enabled = false;
+
+                yield return new WaitForSeconds(duration);
+
+                stats.moveSpeed /= multiplier;
+            }
+        }
+        else
+        {
+            Debug.Log("Thruster failed to be found!");
+        }
+        Destroy(gameObject);
+    }
+}
